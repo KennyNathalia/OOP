67dd91fb7abbde61e31abcd97fc96fb0ca1770d6
Kenny Nathalia <99057679@mydavinci.nl>
2021-11-04 16:16:07 +0100

    added some more stuff to the construct function

--- Pokemon.php
+++ Pokemon.php
@@ -9,11 +9,15 @@
 		public $resistance;
 		public $attack;
 
-		public function __construct($name, $energytype, $hitpoints, $attacks){
+		public function __construct($name, $energytype, $hitpoints, $health, $attacks, $weakness, $resistance, $attack){
 	        $this->name = $name;
 	        $this->energytype = $energytype;
 	        $this->hitpoints = $hitpoints;
+	        $this->health = $health;
 	        $this->attacks = $attacks;
+	        $this->weakness = $weakness;
+	        $this->resistance = $resistance;
+	        $this->attack = $attack;
 	    }
 
 	    public function __toString() {
